607cc08f3e3849ed547e985e596844c6678cca43
Muhammad Jalloh <nodeversity@gmail.com>
2020-06-22 18:36:14 -0500

    Implemented CommentApi object

--- src/api/CommentApi.js
+++ src/api/CommentApi.js
@@ -1,2 +1,25 @@
 import Axios from '../api';
 
+class CommentApi {
+    /**
+     * Handles all CRUD calls made against the http://jsonplaceholder.typicode.com/comments
+     * API endpoint
+     * Caveat: CREATE, UPDATE and PATCH methods are only simulated.
+     */
+
+     static getCommentsForPost(postId){
+         return Axios.get(`comments?postId=${postId}`);
+     }
+
+     static getComment(commentId){
+         return Axios.get(`comments/${commentId}`);
+     }
+
+     static deleteComment(commentId){
+         return Axios.delete(`comments/${commentId}`);
+     }
+
+     static newComment(newCommentObj){
+         return Axios.post(`comments/`, newCommentObj);
+     }
+}
