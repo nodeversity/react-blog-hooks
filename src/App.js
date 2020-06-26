a855eae5a8f551f95dc77cfa7da587f583f8558f
Muhammad Jalloh <nodeversity@gmail.com>
2020-06-25 19:02:02 -0500

    Added primary routes for app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import Home from './components/Home';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
+//Import components to configure app's routing
+import CategoryDetail from './components/blog/CategoryDetail'
+import PostDetail from './components/blog/PostDetail'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <header className="App-header">
         <Header />
       </header>
-      <Home />
+      <Route path="/" component={Home} />
+      <Route path="/:categoryUrl" exact component={CategoryDetail} />
+      <Route path="/:categoryUrl/:postUrl" component={PostDetail} />
       <Footer />
     </div>
+    </Router>
   );
 }
 
